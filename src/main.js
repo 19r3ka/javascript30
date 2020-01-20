571f79af404eea76b9cd03b50271a6ef15394b3d
19r3ka <yemahiatsi@gmail.com>
2020-01-20 09:19:56 +0000

    Adds transitions to keys

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
+const borderColors = [
+  "magenta",
+  "yellow",
+  "aqua",
+  "blue",
+  "orange",
+  "green",
+  "lime",
+  "cornsilk",
+  "maroon",
+  "lavenderBlush"
+];
+
+// select a random color as keys highlight color when pressed
+document.documentElement.style.setProperty(
+  "--border-color",
+  borderColors[Math.floor(Math.random() * borderColors.length)]
+);
+
 const playKey = function({ key }) {
   const audio = document.getElementById(`key-${key}`);
-  if (!audio) return; // Do nothing if audio element doesn't exist
+  const button = document.getElementById(`button-${key}`);
+  if (!audio || !key) return; // Do nothing if audio element doesn't exist
   audio.currentTime = 0; // always restart sound on keydown
   audio.play();
+  button.classList.add("playing");
 };
 
 document.addEventListener("keydown", playKey);
@@ -11,5 +32,9 @@ const buttons = document.getElementsByTagName("button");
 for (let button of buttons) {
   button.addEventListener("click", () =>
     playKey({ key: button.getAttribute("data-key") })
+  );
+
+  button.addEventListener("transitionend", () =>
+    button.classList.remove("playing")
   );
 }
